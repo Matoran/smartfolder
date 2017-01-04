d0f59946dcb8ac816c4f6dd3150ac1511c83d511
Matoran <marco.lopes05@gmail.com>
2017-01-04 22:31:22 +0100

    add linker

--- linker.c
+++ linker.c
@@ -2,18 +2,25 @@
 // Created by matoran on 12/16/16.
 //
 
+#include <memory.h>
+#include <stdio.h>
+#include <zconf.h>
+#include <stdlib.h>
 #include "linker.h"
 
 void zelda(const char *linkSource, const char *filename){
 
-    char *linkDestination = strcat(linker_destination, filename);
+    char *linkDestination = malloc(sizeof(char)*(strlen(linker_destination)+strlen(filename)+10));
+    strcpy(linkDestination, linker_destination);
+    strcat(linkDestination, "/");
+    strcat(linkDestination, filename);
     FILE* file = NULL;
     int i = 1;
     do{
         file = fopen(linkDestination, "r+");
-        strcat(linkDestination, i);
+        linkDestination
         i++;
     }while(file != NULL);
-
+    sprintf(linkDestination, "%d", i);
     symlink(linkSource, linkDestination);
 }
