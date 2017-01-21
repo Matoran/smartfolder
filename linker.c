f4081f31d9ab5580778275b2a1abf83dc9d0f467
lekikou <lekikou@users.noreply.github.com>
2017-01-21 17:56:35 +0100

    Modification du linker avec la fonction exist

--- linker.c
+++ linker.c
@@ -16,21 +16,23 @@
 #include <time.h>
 #include "wrappersyscall.h"
 
-bool isExist(const char *linkSource, char *linkDestination){
+int exist(const char *linkSource, char *linkDestination){
     struct stat sb;
     char *linkname;
 
-    lstatw(linkDestination, &sb);
+    if(lstat(linkDestination, &sb) == -1){
+        return SYMLINK_NO_EXIST;
+    }
     linkname = mallocw(sb.st_size + 1);
     readlinkw(linkDestination, linkname, sb.st_size + 1);
     linkname[sb.st_size] = '\0';
 
     if(strcmp(linkSource,linkname) == 0){
         free(linkname);
-        return true;
+        return SAME_SYMLINK;
     }
     free(linkname);
-    return false;
+    return SYMLINK_EXIST;
 }
 
 void zelda(const char *linkSource, const char *filename){
@@ -38,23 +40,23 @@ void zelda(const char *linkSource, const char *filename){
     strcpy(linkDestination, linker_destination);
     strcat(linkDestination, "/");
     strcat(linkDestination, filename);
-    FILE* file = NULL;
     int i = 0;
     bool open = true;
     size_t length = strlen(linkDestination);
+    int valueExist;
     do{
-        file = fopen(linkDestination, "r+");
-        if(file != NULL) {
-            fclose(file);
-            if(isExist(linkSource,linkDestination)){
-                return;
-            }
+        valueExist = exist(linkSource,linkDestination);
+        if(valueExist == SYMLINK_EXIST) {
             i++;
             sprintf(&linkDestination[length], "%d", i);
-        }else{
+        }else if(valueExist == SYMLINK_NO_EXIST){
             open = false;
+        }else{
+            return;
         }
     }while(open);
+
+    logger("link source %s link destination %s\n", DEBUG,true, linkSource, linkDestination);
 
     symlinkw(linkSource, linkDestination);
     free(linkDestination);
--- linker.h
+++ linker.h
@@ -5,6 +5,10 @@
 #ifndef SEARCHFOLDER_LINKER_H
 #define SEARCHFOLDER_LINKER_H
 
+#define SYMLINK_EXIST 0
+#define SAME_SYMLINK 1
+#define SYMLINK_NO_EXIST 2
+
 char* linker_destination;
 void zelda(const char *linkSource, const char *filename);
 #endif //SEARCHFOLDER_LINKER_H
